71b45116b0aa83c4a223279973037b84fac5c2cb
Reza <kourosh.kabiri2014@gmail.com>
2021-07-12 15:01:19 +0430

    Simple Cornometer

---
+++ js/counter.js
@@ -0,0 +1,26 @@
+let id;
+let second = 0;
+
+
+function printmessage(){
+    document.getElementById('message').innerHTML = second+'Sec'
+    second++;
+}
+
+
+function start(){
+   id = window.setInterval(printmessage,1)
+}
+
+function stop(){
+    window.clearInterval(id)
+}
+function reset(){
+    window.clearInterval(id)
+    second = 0;
+    document.getElementById('message').innerHTML = second + 'Sec'
+    
+    
+    
+}
+
